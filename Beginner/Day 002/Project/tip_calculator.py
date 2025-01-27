3890e1d2e0ce511503a6619152220dcc0c46b997
Anargyros Sigalas <zerotoblockchaindev@gmail.com>
2025-01-27 13:39:24 +0900

    project correction

--- Beginner/Day 002/Project/tip_calculator.py
+++ Beginner/Day 002/Project/tip_calculator.py
@@ -1,5 +1,3 @@
-from xxsubtype import bench
-
 print("Welcome to the tip calculator!")
 bill = float(input("What was the total bill? $"))
 tip = int(input("What percentage tip would you like to give? 10 12 15 "))
