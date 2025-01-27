9ba92491efb63f95453fc1e2e5ee14f5b66dacb6
Anargyros Sigalas <zerotoblockchaindev@gmail.com>
2025-01-27 13:16:58 +0900

    completed day 2

---
+++ Beginner/Day 002/Project/tip_calculator.py
@@ -0,0 +1,15 @@
+from xxsubtype import bench
+
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+people = int(input("How many people to split the bill? "))
+
+total = (bill+(tip / 100 * bill)) # total amount of bill including the tip
+split_amount = round((total / people), 2) # amount of money each will pay
+
+print(f"Each person should pay: ${split_amount:.2f}") # :.2f gives us 2 decimal points, :.1f would give 1 decimal point
+
+# Another way to do it would be
+# split_amount= "{:.2f}".format(total / people)
+# print(f"Each person should pay: ${split_amount}")
