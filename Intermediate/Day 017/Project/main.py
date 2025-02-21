b8097826703c183babd5b58423e710ccd50d49c7
Anargyros Sigalas <asigalas.dev@gmail.com>
2025-02-21 14:44:20 +0900

    day 17 quiz game main file

---
+++ Intermediate/Day 017/Project/main.py
@@ -0,0 +1,18 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+for q in question_data:
+    question_bank.append(Question(q["text"], q["answer"]))
+
+# same as above but shorter
+# question_bank = [Question(q['text'], q['answer']) for q in question_data]
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz.")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
